dca37ff2188297704b02190dec49237f49446bfd
Andrew Rich <andrew.rich@gmail.com>
2024-07-21 18:30:04 -0700

    fmt

--- main.tf
+++ main.tf
@@ -11,9 +11,9 @@ module "networking" {
 module "container_service" {
   source = "./modules/container_service"
 
-  project_name      = "ar-sre-kata"
-  vpc_id            = module.networking.vpc_id
-  public_subnet_ids = module.networking.public_subnet_ids
+  project_name       = "ar-sre-kata"
+  vpc_id             = module.networking.vpc_id
+  public_subnet_ids  = module.networking.public_subnet_ids
   private_subnet_ids = module.networking.private_subnet_ids
   tags = {
     Owner = "AR"
