e4b4d6574eca96d1d82880b8ded7b15f09a226eb
Andrew Rich <andrew.rich@gmail.com>
2024-07-21 20:40:41 -0700

    use private static IP for Redis

--- modules/container_service/outputs.tf
+++ modules/container_service/outputs.tf
@@ -1,5 +1,1 @@
 # ./modules/container_service/outputs.tf
-output "redis_private_ip" {
-  description = "Private IP of the Redis container"
-  value       = aws_ecs_task_definition.redis.network_configuration[0].private_ip_address
-}
