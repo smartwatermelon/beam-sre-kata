23ae3d1eb0cd3e1180c5bfd1ca97b29076ee62be
Andrew Rich <andrew.rich@gmail.com>
2024-07-23 14:56:07 -0700

    additional/enhanced unit tests

--- modules/serverless/lambda/test_lambda.rb
+++ modules/serverless/lambda/test_lambda.rb
@@ -1,27 +1,63 @@
-# modules/serverless/lambda/test_lambda.rb
+# ./modules/serverless/lambda/test_lambda.rb
+
 require 'bundler/setup'
 require 'minitest'
+require 'json'
 require_relative 'index'
 
 class TestLambda < Minitest::Test
+  def setup
+    # Mock the File.read method to return a sample config
+    File.stub :read, '{"api_url":"https://test.api","city":"TestCity","state":"TestState","per_page":10}' do
+      @config = load_config
+    end
+  end
+
+  def test_load_config
+    assert_equal 'https://test.api', @config['api_url']
+    assert_equal 'TestCity', @config['city']
+    assert_equal 'TestState', @config['state']
+    assert_equal 10, @config['per_page']
+  end
 
   def test_fetch_breweries
-    config = { 'api_url' => 'https://api.openbrewerydb.org/v1/breweries', 'city' => 'Columbus', 'state' => 'Ohio', 'per_page' => 50 }
-    breweries = fetch_breweries(config)
-    assert_instance_of Array, breweries
-    refute_empty breweries
-    assert breweries.all? { |b| b['city'] == 'Columbus' && b['state'] == 'Ohio' }
+    # Mock Net::HTTP to return a sample response
+    mock_response = '{"name":"Test Brewery","city":"TestCity","state":"TestState"}'
+    Net::HTTP.stub :get_response, OpenStruct.new(body: mock_response, code: '200') do
+      breweries = fetch_breweries(@config)
+      assert_instance_of Array, breweries
+      refute_empty breweries
+      assert breweries.all? { |b| b['city'] == 'TestCity' && b['state'] == 'TestState' }
+    end
   end
 
   def test_format_brewery_data
     breweries = [
       { 'name' => 'Test Brewery', 'street' => '123 Test St', 'phone' => '1234567890' },
-      { 'name' => 'Another Brewery', 'street' => '456 Another St', 'phone' => '0987654321' }
+      { 'name' => 'Another Brewery', 'street' => '456 Another St', 'phone' => '0987654321' },
+      { 'name' => 'Zeta Brewery', 'street' => '789 Zeta St', 'phone' => '5555555555' }
     ]
     formatted = format_brewery_data(breweries)
-    assert_equal 2, formatted.length
-    assert_equal ['Another Brewery', 'Test Brewery'], formatted.map { |b| b[:name] }
+    assert_equal 3, formatted.length
+    assert_equal ['Another Brewery', 'Test Brewery', 'Zeta Brewery'], formatted.map { |b| b[:name] }
     assert formatted.all? { |b| [:name, :street, :phone].all? { |k| b.key?(k) } }
+  end
+
+  def test_handler
+    event = {}
+    context = nil
+    
+    # Mock the necessary methods
+    File.stub :read, '{"api_url":"https://test.api","city":"TestCity","state":"TestState","per_page":10}' do
+      Net::HTTP.stub :get_response, OpenStruct.new(body: '[{"name":"Test Brewery","city":"TestCity","state":"TestState","street":"Test St","phone":"1234567890"}]', code: '200') do
+        result = handler(event: event, context: context)
+        assert_equal 200, result[:statusCode]
+        body = JSON.parse(result[:body])
+        assert_instance_of Array, body
+        assert_equal 1, body.length
+        assert_equal 'Test Brewery', body[0]['name']
+      end
+    end
   end
 
   def self.run_tests
