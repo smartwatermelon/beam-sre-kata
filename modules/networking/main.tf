3557efed4c229bcecbed753ad41dd98c5b00c607
Andrew Rich <andrew.rich@gmail.com>
2024-07-24 09:23:20 -0700

    comments and logical reordering

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -1,4 +1,5 @@
 # ./modules/networking/main.tf
+
 # Create VPC
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
@@ -25,26 +26,18 @@ resource "aws_internet_gateway" "main" {
   )
 }
 
-# Elastic IP for NAT Gateway
-
+# Create Elastic IP for NAT Gateway
 resource "aws_eip" "nat" {
   domain = "vpc"
   count  = 1
   tags   = merge(var.tags, { Name = "${var.project_name}-nat-eip" })
 }
 
-# NAT Gateway
+# Create NAT Gateway
 resource "aws_nat_gateway" "main" {
   allocation_id = aws_eip.nat[0].id
   subnet_id     = aws_subnet.public[0].id
   tags          = merge(var.tags, { Name = "${var.project_name}-nat-gateway" })
-}
-
-# Update private route table to route through NAT Gateway
-resource "aws_route" "private_nat_gateway" {
-  route_table_id         = aws_route_table.private.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.main.id
 }
 
 # Create public subnets
@@ -92,12 +85,6 @@ resource "aws_subnet" "redis" {
   )
 }
 
-# Associate the Redis subnet with the private route table
-resource "aws_route_table_association" "redis" {
-  subnet_id      = aws_subnet.redis.id
-  route_table_id = aws_route_table.private.id
-}
-
 # Create public route table
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
@@ -134,9 +121,22 @@ resource "aws_route_table" "private" {
   )
 }
 
+# Update private route table to route through NAT Gateway
+resource "aws_route" "private_nat_gateway" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.main.id
+}
+
 # Associate private subnets with private route table
 resource "aws_route_table_association" "private" {
   count          = length(aws_subnet.private)
   subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+# Associate the Redis subnet with the private route table
+resource "aws_route_table_association" "redis" {
+  subnet_id      = aws_subnet.redis.id
   route_table_id = aws_route_table.private.id
 }
